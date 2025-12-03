27a7a1c7355c291aaca29173fcaf77ab1596bec4
Talha Rana <mtalharana093@gmail.com>
2025-12-04 00:49:22 +0500

    create alias in dependency injection

--- app/main.py
+++ app/main.py
@@ -4,24 +4,37 @@ from typing import Annotated
 app = FastAPI()
 
 
-# # Creating Dependency Function
-# async def common_parameters(q:str | None =None , skip: int = 0, limit: int = 100):
-#   return {"q": q, "skip": skip, "limit": limit}
+# Creating Dependency Function
+async def common_parameters(q:str | None =None , skip: int = 0, limit: int = 100):
+  return {"q": q, "skip": skip, "limit": limit}
 
 
-# # Using Dependency in endpoints
+# Using Dependency in endpoints
 
-# @app.get("/items")
-# # defined parameters of route function main likhna Depends() main 
-# async def get_items(commons: Annotated[dict, Depends(common_parameters)]):
-#   return commons
+@app.get("/items")
+# defined parameters of route function main likhna Depends() main 
+async def get_items(commons: Annotated[dict, Depends(common_parameters)]):
+  return commons
 
 
-# @app.get("/users")
-# # defined parameters of route function main likhna Depends() main 
-# async def get_users(commons: Annotated[dict, Depends(common_parameters)]):
-#   return commons
+@app.get("/users")
+# defined parameters of route function main likhna Depends() main 
+async def get_users(commons: Annotated[dict, Depends(common_parameters)]):
+  return commons
 
+
+# Create a type alias
+CommonDep = Annotated[dict, Depends(common_parameters)]
+
+@app.get("/products")
+# defined parameters of route function main likhna Depends() main 
+async def get_products(commons: CommonDep):
+  return commons
+
+@app.get("/posts")
+# defined parameters of route function main likhna Depends() main 
+async def get_posts(commons: CommonDep):
+  return commons
 
 # def current_user():
 #     return {"name": "Talha"}
